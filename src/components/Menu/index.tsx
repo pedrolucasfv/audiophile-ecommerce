edc46fbb46722d6b4cfef49a8dbb80deccdf770f
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-01-26 13:33:10 -0300

    feat: Add component Menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,51 @@
+import MediaMatch from 'components/MediaMatch'
+import * as S from './styles'
+
+import { Cart } from '@styled-icons/bootstrap/Cart'
+import { PersonFill } from '@styled-icons/bootstrap/PersonFill'
+import { MenuButtonWide } from '@styled-icons/bootstrap/MenuButtonWide'
+import { ArrowDownSquare } from '@styled-icons/bootstrap/ArrowDownSquare'
+
+import { useState } from 'react'
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <S.Wrapper>
+      <MediaMatch lessThan="medium" onClick={() => setIsOpen(true)}>
+        <MenuButtonWide size={18} color="white" />
+      </MediaMatch>
+
+      <S.Logo />
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">home</S.MenuLink>
+          <S.MenuLink href="#">headphones</S.MenuLink>
+          <S.MenuLink href="#">speakers</S.MenuLink>
+          <S.MenuLink href="#">earphones</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
+      <S.MenuGroup>
+        <PersonFill size={20} />
+        <Cart size={20} />
+      </S.MenuGroup>
+
+      <S.MenuToggle aria-hidden={!isOpen} isOpenMenu={isOpen}>
+        <ArrowDownSquare
+          aria-label="Close Menu"
+          onClick={() => setIsOpen(false)}
+        />
+        <S.MenuNav>
+          <S.MenuLink href="#">Colletions</S.MenuLink>
+          <S.MenuLink href="#">Men</S.MenuLink>
+          <S.MenuLink href="#">Women</S.MenuLink>
+          <S.MenuLink href="#">About</S.MenuLink>
+          <S.MenuLink href="#">Contact</S.MenuLink>
+        </S.MenuNav>
+      </S.MenuToggle>
+    </S.Wrapper>
+  )
+}
+export default Menu
---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,95 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.main`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: ${theme.spacings.small} 0;
+  `}
+`
+
+export const Logo = styled.div`
+  margin-top: 0.6rem;
+  height: 5rem;
+  width: 15rem;
+  background-image: url('/img/logo-audiophile.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+`
+
+export const MenuNav = styled.div``
+
+export const MenuLink = styled.a`
+  ${({ theme }) => css`
+    color: white;
+    font-size: 1.4rem;
+    margin: 0.3rem 2.8rem 0;
+    text-decoration: none;
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+    text-align: center;
+    &:hover {
+      color: ${theme.colors.accent};
+    }
+  `}
+`
+
+export const MenuGroup = styled.div`
+  ${({ theme }) => css`
+    color: white;
+    svg {
+      margin-right: 1rem;
+      cursor: pointer;
+      &:hover {
+        color: ${theme.colors.accent};
+      }
+    }
+  `}
+`
+
+type MenuToggleProps = {
+  isOpenMenu: boolean
+}
+export const MenuToggle = styled.nav<MenuToggleProps>`
+  ${({ isOpenMenu }) => css`
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: fixed;
+    z-index: 20;
+    width: 42%;
+    left: 0;
+    top: 0;
+
+    bottom: 0;
+    height: 100vh;
+    transition: opacity 0.3s ease-in-out;
+    opacity: ${isOpenMenu ? 1 : 0};
+    pointer-events: ${isOpenMenu ? 'all' : 'none'};
+
+    > svg {
+      margin: 1.5rem;
+      cursor: pointer;
+      width: 2rem;
+      height: 2rem;
+    }
+    ${MenuNav} {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+      padding: 1rem;
+      flex: 1;
+    }
+    ${MenuLink} {
+      color: black;
+      font-weight: bold;
+      font-size: 1.2rem;
+      margin-bottom: 1rem;
+      transform: ${isOpenMenu ? 'translateY(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
+    }
+  `}
+`
