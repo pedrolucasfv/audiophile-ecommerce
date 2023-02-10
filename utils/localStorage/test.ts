5a24899ae08fd917a4aa49b166d1a6d12fc5005d
tuktuks <pedro.lucas.tukinha25@gmail.com>
2023-02-09 22:56:49 -0300

    feat: add method localStorage em utils

---
+++ utils/localStorage/test.ts
@@ -0,0 +1,24 @@
+import { getStorageItem, setStorageItem } from '.'
+
+describe('getStorageItem', () => {
+  it('should return the item from localStorage', () => {
+    window.localStorage.setItem(
+      'AUDIOPHILE_cartItems',
+      JSON.stringify(['1', '2'])
+    )
+    expect(getStorageItem('cartItems')).toStrictEqual(['1', '2'])
+  })
+})
+
+describe('setStorageItem', () => {
+  beforeEach(() => {
+    window.localStorage.clear()
+  })
+  it('should add the item to localStorage', () => {
+    setStorageItem('cartItems', ['1', '2'])
+
+    expect(window.localStorage.getItem('AUDIOPHILE_cartItems')).toStrictEqual(
+      JSON.stringify(['1', '2'])
+    )
+  })
+})
