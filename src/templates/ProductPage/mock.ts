e8b59f7574b59dfcb6dcfc44dd73b08c8e80228f
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-02-28 16:04:56 -0300

    feat: adicionando as páginas de produtos com slug

---
+++ src/templates/ProductPage/mock.ts
@@ -0,0 +1,115 @@
+export default [
+  {
+    slug: 'XX99-MARK-II-HEADPHONES',
+    product: {
+      title: 'new product',
+      name: 'XX99 MARK II HEADPHONES',
+      description:
+        'The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.',
+      price: 2.999,
+      image: '/img/image-product.jpg'
+    },
+    productInfo: {
+      text: 'Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening. It includes intuitive controls designed for any situation. Whether you’re taking a business call or just in your own personal space, the auto on/off and pause features ensure that you’ll never miss a beat.',
+      items: [
+        {
+          item: 'Headphone Unit',
+          quantity: 1
+        },
+        {
+          item: 'Replacement Earcups',
+          quantity: 2
+        },
+        {
+          item: 'User Manual',
+          quantity: 1
+        },
+        {
+          item: '3.5mm 5m Audio Cable',
+          quantity: 1
+        },
+        {
+          item: 'Travel Bag',
+          quantity: 1
+        }
+      ]
+    },
+    categoryCard: {
+      image: '/img/image-headphones.png',
+      link: '/',
+      title: 'HEADPHONES'
+    }
+  },
+  {
+    slug: 'XX59-HEADPHONES',
+    product: {
+      name: 'XX59 HEADPHONES',
+      description:
+        'Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.',
+      price: 899,
+      image: '/img/product-xx59-headphones/desktop/image-product.jpg'
+    },
+    productInfo: {
+      text: 'These headphones have been created from durable, high-quality materials tough enough to take anywhere. Its compact folding design fuses comfort and minimalist style making it perfect for travel. Flawless transmission is assured by the latest wireless technology engineered for audio synchronization with videos. More than a simple pair of headphones, this headset features a pair of built-in microphones for clear, hands-free calling when paired with a compatible smartphone. Controlling music and calls is also intuitive thanks to easy-access touch buttons on the earcups. Regardless of how you use the XX59 headphones, you can do so all day thanks to an impressive 30-hour battery life that can be rapidly recharged via USB-C.',
+      items: [
+        {
+          item: 'Headphone Unit',
+          quantity: 1
+        },
+        {
+          item: 'Replacement Earcups',
+          quantity: 2
+        },
+        {
+          item: 'User Manual',
+          quantity: 1
+        },
+        {
+          item: '3.5mm 5m Audio Cable',
+          quantity: 1
+        }
+      ]
+    },
+    categoryCard: {
+      image: '/img/image-headphones.png',
+      link: '/',
+      title: 'HEADPHONES'
+    }
+  },
+  {
+    slug: 'XX99-MARK-I-HEADPHONES',
+    product: {
+      name: 'XX99 MARK I HEADPHONES',
+      description:
+        'As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.',
+      price: 1750,
+      image: '/img/product-xx99-mark-one-headphones/desktop/image-product.jpg'
+    },
+    productInfo: {
+      text: 'As the headphones all others are measured against, the XX99 Mark I demonstrates over five decades of audio expertise, redefining the critical listening experience. This pair of closed-back headphones are made of industrial, aerospace-grade materials to emphasize durability at a relatively light weight of 11 oz. From the handcrafted microfiber ear cushions to the robust metal headband with inner damping element, the components work together to deliver comfort and uncompromising sound. Its closed-back design delivers up to 27 dB of passive noise cancellation, reducing resonance by reflecting sound to a dedicated absorber. For connectivity, a specially tuned cable is includes with a balanced gold connector.',
+      items: [
+        {
+          item: 'Headphone Unit',
+          quantity: 1
+        },
+        {
+          item: 'Replacement Earcups',
+          quantity: 2
+        },
+        {
+          item: 'User Manual',
+          quantity: 1
+        },
+        {
+          item: '3.5mm 5m Audio Cable',
+          quantity: 1
+        }
+      ]
+    },
+    categoryCard: {
+      image: '/img/image-headphones.png',
+      link: '/',
+      title: 'HEADPHONES'
+    }
+  }
+]
