227269d7495f5ac5b629048401fec15099c35bb1
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-03-04 16:05:35 -0300

    feat: estilizando o scroll

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,6 +18,17 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
+  body::-webkit-scrollbar{
+    width: 0.8rem;
+  }
+  body::-webkit-scrollbar-track{
+    background: #303030;
+    box-shadow: inset 0.05rem 0 0 #838383;
+  }
+  body::-webkit-scrollbar-thumb{
+    background: #d87d4a;
+    box-shadow: inset 0.15rem 0.1rem 0.1rem rgba(255,255,255,0.5), inset -0.15rem -0.1rem 0.1rem rgba(0,0,0,0.3);
+  }
 `
 
 export default GlobalStyles
