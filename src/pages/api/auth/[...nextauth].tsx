2dc3cf1ab65bd68a3beb30553bca0957fc0cd729
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-03-16 12:11:54 -0300

    build: consertando erro de typescript

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -8,18 +8,24 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      issuer: process.env.AUTH0_ISSUER
+      clientId: process.env.AUTH0_CLIENT_ID ? process.env.AUTH0_CLIENT_ID : '',
+      clientSecret: process.env.AUTH0_CLIENT_SECRET
+        ? process.env.AUTH0_CLIENT_SECRET
+        : '',
+      issuer: process.env.AUTH0_ISSUER ? process.env.AUTH0_ISSUER : ''
     }),
     // ...add more providers here
     GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientId: process.env.GITHUB_ID ? process.env.GITHUB_ID : '',
+      clientSecret: process.env.GITHUB_SECRET ? process.env.GITHUB_SECRET : ''
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID
+        ? process.env.GOOGLE_CLIENT_ID
+        : '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        ? process.env.GOOGLE_CLIENT_SECRET
+        : ''
     })
   ]
 }
