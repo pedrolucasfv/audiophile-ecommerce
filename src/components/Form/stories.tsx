5bf6c3f498e655de82e254d3c746698873fb4685
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-04-06 12:08:10 -0300

    fix: error stories

--- src/components/Form/stories.tsx
+++ src/components/Form/stories.tsx
@@ -1,9 +1,20 @@
-import { Story, Meta } from '@storybook/react'
-import Form from '.'
+import { Meta, Story } from '@storybook/react'
+import Form, { FormProps } from '.'
 
 export default {
   title: 'Form',
-  component: Form
+  component: Form,
+  args: {
+    productSelected: {
+      name: '',
+      price: 0,
+      description: '',
+      productID: '',
+      category: '',
+      image: ''
+    },
+    type: 'add'
+  }
 } as Meta
 
-export const Default: Story = () => <Form />
+export const Default: Story<FormProps> = (args) => <Form {...args} />
